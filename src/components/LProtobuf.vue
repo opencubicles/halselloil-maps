d4627d7724b1d67c8e52e6dfc3e76a3621dde488
Ankush <ankush@opencubicles.com>
2022-04-12 19:42:42 +0530

    changes

--- src/components/LProtobuf.vue
+++ src/components/LProtobuf.vue
@@ -6,13 +6,9 @@ import {
   WINDOW_OR_GLOBAL,
   GLOBAL_LEAFLET_OPT,
 } from "@vue-leaflet/vue-leaflet/src/utils";
-import {
-  props,
-  setup as vectorLayerSetup,
-} from "@vue-leaflet/vue-leaflet/src/functions/layerGroup";
+import { setup as vectorLayerSetup } from "@vue-leaflet/vue-leaflet/src/functions/layerGroup";
 import L from "leaflet";
 import "leaflet.vectorgrid";
-
 export default {
   props: {
     url: String,
@@ -22,7 +18,7 @@ export default {
     const leafletRef = ref({});
     const useGlobalLeaflet = inject(GLOBAL_LEAFLET_OPT);
     const addLayer = inject("addLayer");
-    const { options, methods } = vectorLayerSetup(props, leafletRef, context);
+    const { methods } = vectorLayerSetup(props, leafletRef, context);
     onMounted(async () => {
       const { DomEvent } = useGlobalLeaflet
         ? WINDOW_OR_GLOBAL.L
@@ -33,15 +29,15 @@ export default {
           interactive: true,
           vectorTileLayerStyles: {
             geojsonLayer: {
-              weight: 0,
-              fillColor: "#000",
-              fillOpacity: 0.5,
+              weight: 1,
+              fillColor: "white",
+              fillOpacity: 0,
               fill: true,
+              color: "black",
             },
           },
         }
       );
-
       const listeners = remapEvents(context.attrs);
       DomEvent.on(leafletRef.value, listeners);
       propsBinder(methods, leafletRef.value, props);
